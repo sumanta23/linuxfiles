ff73be4ded5f1eaa540202ba8178e5f11416262f
sumanta23 <nandisumanta0@gmail.com>
2015-05-16 22:12:59 +0530

    setting env variables

---
+++ etc/setenv.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export JAVA_HOME=/opt/sw/jdk1.7.0_75
+export M2_HOME=/opt/sw/apache-maven-3.3.3
+export ANT_HOME=/opt/sw/apache-ant-1.9.4
+export JBOSS_HOME=/home/sam/software/jboss-as-7.1.1.Final
+export IDEA_HOME=/opt/sw/idea-IC-139.1117.1
+export GRADLE_HOME=/opt/sw/gradle-2.3
+PATH=$PATH:$JAVA_HOME/bin:$M2_HOME/bin:$ANT_HOME/bin:$JBOSS_HOME/bin:$IDEA_HOME/bin:$GRADLE_HOME/bin
